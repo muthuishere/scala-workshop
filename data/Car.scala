1d34a741279c8327e9f0c6fcd26cb6ac209a4a73
Muthukumaran N <muthuishere@gmail.com>
2019-12-31 23:43:20 +0530

    Updates

--- data/Car.scala
+++
@@ -1,9 +0,0 @@
-package functional
-
-
-
-class Car (var sNo:Int,var name:String,var manufacturer:String,var year:Int,var fuelType:String,var transmission:String){
-
-
-  override def toString = s"Car($sNo, $name, $manufacturer, $year, $fuelType, $transmission)"
-}
