345e9bfe521e5404ffce3383f8abce0855efece4
Muthukumaran N <muthuishere@gmail.com>
2020-01-02 15:40:46 +0530

    Updates

--- programs/vehicle-list/app/controllers/VehicleController.scala
+++ programs/vehicle-list/app/controllers/VehicleController.scala
@@ -2,6 +2,9 @@ package controllers
 
 import javax.inject._
 import models.Vehicle
+import play.api.data._
+import play.api.data.Forms._
+import play.api.i18n.I18nSupport
 import play.api.mvc._
 
 /**
@@ -9,7 +12,7 @@ import play.api.mvc._
  * application's home page.
  */
 @Singleton
-class VehicleController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class VehicleController @Inject()(val controllerComponents: ControllerComponents) extends BaseController with  I18nSupport {
 
   /**
    * Create an Action to render an HTML page.
@@ -20,6 +23,27 @@ class VehicleController @Inject()(val controllerComponents: ControllerComponents
    */
   def index() = Action { implicit request: Request[AnyContent] =>
 
-    Ok(views.html.vehicle(Vehicle.list()))
+    Ok(views.html.vehicle(Vehicle.list(),vehicleForm))
   }
+  //case class Vehicle (var sNo:Int,var name:String,var manufacturer:String
+  // ,var year:Int,var fuelType:String,var transmission:String)
+
+  def newVehicle = Action { implicit request=>
+
+    vehicleForm.bindFromRequest.fold(
+      errors=> BadRequest(views.html.vehicle(Vehicle.list(),errors)),
+      current => {
+        Vehicle.create(current)
+        Redirect(routes.VehicleController.index)
+      }
+    )
+  }
+
+  val vehicleForm = Form( mapping (
+    "sNo" -> number,
+    "name" -> text,
+    "manufacturer" -> text,
+    "year" -> number,
+    "fuelType" -> text,
+    "transmission" -> text)(Vehicle.apply)(Vehicle.unapply))
 }
--- programs/vehicle-list/app/models/Vehicle.scala
+++ programs/vehicle-list/app/models/Vehicle.scala
@@ -2,13 +2,18 @@ package models
 
 import providers.VehicleMockDataProvider
 
+import scala.collection.mutable.ListBuffer
+
 case class Vehicle (var sNo:Int,var name:String,var manufacturer:String,var year:Int,var fuelType:String,var transmission:String)
 
 object Vehicle{
 
-  var vehicles = VehicleMockDataProvider.getAllVehicles()
+  var vehicles:ListBuffer[Vehicle] = ListBuffer.empty ++ VehicleMockDataProvider.getAllVehicles()
 
   def list(): List[Vehicle] = {
-   return vehicles
+   return vehicles.toList
+  }
+  def create(vehicle:Vehicle) ={
+    vehicles += vehicle
   }
 }
