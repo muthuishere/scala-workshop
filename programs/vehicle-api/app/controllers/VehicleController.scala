eb05d590118e934d087ba3e046e4c0f8f6f7479f
Muthukumaran N <muthuishere@gmail.com>
2020-01-02 16:43:23 +0530

    Create API

--- programs/vehicle-api/app/controllers/VehicleController.scala
+++ programs/vehicle-api/app/controllers/VehicleController.scala
@@ -6,16 +6,22 @@ import play.api.libs.json.Json
 import play.api.mvc._
 
 /**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+  * This controller creates an `Action` to handle HTTP requests to the
+  * application's home page.
+  */
 @Singleton
 class VehicleController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
 
-  def list = Action{ implicit request =>
+  def list = Action { implicit request =>
 
     Ok(Json.toJson(Vehicle.list()))
   }
 
+  def create() = Action { implicit request =>
+    val jsonInput = request.body.asJson
+    val convertedVehicleFromJson = jsonInput.get.as[Vehicle]
+    Vehicle.create(convertedVehicleFromJson)
+    Ok(Json.toJson(Vehicle.list()))
+  }
 }
